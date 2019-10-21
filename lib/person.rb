645c31bea8cfe242dc8c496a1370f5bcd70fb470
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 05:57:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,3 +5,17 @@ class Person
     attributes.each {|key, value|  self.send(("#{key}="), value)}
   end
 end
+
+
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+ 
+bob = Person.new(bob_attributes)
+bob.name       
+bob.hair_color 
+ 
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+ 
+susan = Person.new(susan_attributes)
+susan.name      
+susan.height    
+susan.eye_color
